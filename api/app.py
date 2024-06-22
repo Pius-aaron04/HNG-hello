4221265cc4f17010865ca64b699e1bb33da2be78
Pius Aaron <piuschbz@gmail.com>
2024-06-22 11:37:59 +0100

    removed cache files

--- api/app.py
+++ api/app.py
@@ -5,10 +5,11 @@ from api import app
 
 @app.route("/api/hello", strict_slashes=False)
 def say_hello():
-    visitor = request.args.get("visitor_name") or "there"
-    client_ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+    visitor = request.args.get("visitor_name").replace('"', '').replace("'", '') or "there"
+    client_ip = request.headers.get('X-Forwarded-For').split(',')[0].strip() or request.remote_addr
     response = requests.get('http://ipinfo.io/{}/json'.format(client_ip)).json()
 
+    print(client_ip)
     return jsonify({
         "client_ip": response.get('ip'),
         "location": response.get('city'),
