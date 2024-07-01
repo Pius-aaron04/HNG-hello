25255aaedd98c4eb07d30d22d323801b9cfa2f6e
Pius Aaron <piuschbz@gmail.com>
2024-07-01 11:58:51 +0100

    Procfile to run gunicorn

--- api/app.py
+++ api/app.py
@@ -3,6 +3,10 @@ import requests
 
 from api import app
 
+@app.route("/", strict_slashes=False)
+def index():
+    return "<h1>Hello, there!</h1>"
+
 @app.route("/api/hello", strict_slashes=False)
 def say_hello():
     visitor = request.args.get("visitor_name").replace('"', '').replace("'", '') or "there"
@@ -13,7 +17,6 @@ def say_hello():
         client_ip = request.remote_addr
     response = requests.get('http://ipinfo.io/{}/json'.format(client_ip)).json()
 
-    print(client_ip)
     return jsonify({
         "client_ip": response.get('ip'),
         "location": response.get('city'),
